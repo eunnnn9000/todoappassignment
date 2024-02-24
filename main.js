656e8892eec2c2bd8221779303a8f971eef8979f
esther <eunnnn9000@gmail.com>
2024-02-23 20:40:48 -0700

    commit

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-const input = document.getElementById('taskInput');
-const button = document.getElementById('addItemBtn');
-const row = document.querySelector('.row'); // Using querySelector to select the first element with the class name "row"
+const row=document.querySelector('.row');
+const button=document.getElementById('addItemBtn');
+const inputValue=document.getElementById('taskInput');
 
-button.addEventListener('click', function() {
-    const inputValue = input.value.trim();
 
-    if (inputValue !== '') {
-        const newContent = document.createElement('div');
-        newContent.style.backgroundColor = 'gray';
-        newContent.style.padding = '50px';
-        newContent.textContent = inputValue;
-        row.appendChild(newContent); // Appending to the selected row element
-        input.value = '';
-    }
-});
